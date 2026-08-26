cc23ab6bd68bdee78843e82ae52a295ef9fb03ed
guoyg <guoyangguang01@gmail.com>
2026-08-26 16:42:33 +0800

    fix: add component scan and entity scan for multi-module JPA
    - Add @ComponentScan to scan all sdncustom packages
    - Add @EntityScan for common.model package
    - Add @EnableJpaRepositories for server.repository package

--- sdncustom-server/src/main/java/com/sdncustom/server/SdncustomApplication.java
+++ sdncustom-server/src/main/java/com/sdncustom/server/SdncustomApplication.java
@@ -2,10 +2,16 @@ package com.sdncustom.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ComponentScan("com.sdncustom")
+@EntityScan("com.sdncustom.common.model")
+@EnableJpaRepositories("com.sdncustom.server.repository")
 public class SdncustomApplication {
 
     public static void main(String[] args) {
